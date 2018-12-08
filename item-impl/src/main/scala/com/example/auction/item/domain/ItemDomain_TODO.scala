de3ac6f4936b96b9c0222f6a56b43525947d50c6
Nico Serfontein <nserfontein@ebay.com>
2018-12-08 22:58:06 +0100

    Update

---
+++ item-impl/src/main/scala/com/example/auction/item/domain/ItemDomain_TODO.scala
@@ -0,0 +1,56 @@
+package com.example.auction.item.domain
+
+import com.example.auction.item.impl._
+import akka.Done
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity
+import com.lightbend.lagom.scaladsl.pubsub.{PubSubRegistry, TopicId}
+
+trait ItemDomain_TODO {
+  this: ItemEntity with PersistentEntity =>
+
+  def pubSubRegistry: PubSubRegistry
+
+  def initialState: ItemAggregate = ???
+
+  def onCreateItem(command: CreateItem, aggregate: ItemAggregate, ctx: CommandContext[Done]): Persist = {
+  ???
+}
+
+def onStartAuction(command: StartAuction, aggregate: ItemAggregate, ctx: CommandContext[Done]): Persist = {
+  ???
+}
+
+def onUpdatePrice(command: UpdatePrice, aggregate: ItemAggregate, ctx: CommandContext[Done]): Persist = {
+  ???
+}
+
+def onFinishAuction(command: FinishAuction, aggregate: ItemAggregate, ctx: CommandContext[Done]): Persist = {
+  ???
+}
+
+
+  def onGetItem(query: GetItem.type, aggregate: ItemAggregate, ctx: ReadOnlyCommandContext[ItemState]): Unit = {
+  ???
+}
+
+
+  def onItemCreated(event: ItemCreated, aggregate: ItemAggregate): ItemAggregate = {
+  ???
+}
+
+def onAuctionStarted(event: AuctionStarted, aggregate: ItemAggregate): ItemAggregate = {
+  ???
+}
+
+def onPriceUpdated(event: PriceUpdated, aggregate: ItemAggregate): ItemAggregate = {
+  ???
+}
+
+def onAuctionFinished(event: AuctionFinished, aggregate: ItemAggregate): ItemAggregate = {
+  ???
+}
+
+
+
+}
+
