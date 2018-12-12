ec8e393601a203fcdc6fecc381690cca34963ced
Nico Serfontein <nserfontein@ebay.com>
2018-12-12 15:27:30 +0100

    Update

--- item-impl/src/main/scala/com/example/auction/item/domain/ItemEventProcessor.scala
+++ item-impl/src/main/scala/com/example/auction/item/domain/ItemEventProcessor.scala
@@ -1,32 +1,31 @@
 package com.example.auction.item.domain
 
-import com.example.auction.item.impl._
 import com.datastax.driver.core.{BoundStatement, PreparedStatement}
+import com.example.auction.item.impl._
+
 import scala.concurrent.Future
 
 trait ItemEventProcessor {
 
-   def insertItemCreator: PreparedStatement
- def insertItemSummaryByCreator: PreparedStatement
+  def insertItemCreator: PreparedStatement
 
+  def insertItemSummaryByCreator: PreparedStatement
 
   def processItemCreated(event: ItemCreated): Future[List[BoundStatement]] = {
-  Future.successful(List.empty)
-}
+    Future.successful(List.empty)
+  }
 
-def processAuctionStarted(event: AuctionStarted): Future[List[BoundStatement]] = {
-  Future.successful(List.empty)
-}
+  def processAuctionStarted(event: AuctionStarted): Future[List[BoundStatement]] = {
+    Future.successful(List.empty)
+  }
 
-def processPriceUpdated(event: PriceUpdated): Future[List[BoundStatement]] = {
-  Future.successful(List.empty)
-}
+  def processPriceUpdated(event: PriceUpdated): Future[List[BoundStatement]] = {
+    Future.successful(List.empty)
+  }
 
-def processAuctionFinished(event: AuctionFinished): Future[List[BoundStatement]] = {
-  Future.successful(List.empty)
-}
-
-
+  def processAuctionFinished(event: AuctionFinished): Future[List[BoundStatement]] = {
+    Future.successful(List.empty)
+  }
 
 }
 
--- item-impl/src/test/scala/com/example/auction/item/domain/ItemEventProcessorSpec.scala
+++ item-impl/src/test/scala/com/example/auction/item/domain/ItemEventProcessorSpec.scala
@@ -1,7 +1,32 @@
 package com.example.auction.item.domain
 
-import org.scalatest.AsyncWordSpec
+import com.example.auction.item.impl.ItemApplication
+import com.lightbend.lagom.scaladsl.api.ServiceLocator
+import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
+import com.lightbend.lagom.scaladsl.persistence.ReadSide
+import com.lightbend.lagom.scaladsl.testkit.{ReadSideTestDriver, ServiceTest}
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
 
-class ItemEventProcessorSpec extends AsyncWordSpec {
+class ItemEventProcessorSpec extends AsyncWordSpec with BeforeAndAfterAll with Matchers {
+
+  val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra()) { ctx =>
+    new ItemApplication(ctx) {
+
+      override def serviceLocator: ServiceLocator = NoServiceLocator
+
+      override lazy val readSide: ReadSide = new ReadSideTestDriver()
+
+    }
+  }
+
+  override def afterAll(): Unit = server.stop()
+
+  val testDriver = server.application.readSide
+
+  "The item event processor" should {
+    "create an item" in {
+      pending
+    }
+  }
 
 }
