9b8c0d1b119787b6495b3fb66d6fa127e9d085a3
Nico Serfontein <nserfontein@ebay.com>
2018-12-17 20:15:51 +0100

    Update

--- item-impl/src/test/scala/com/example/auction/item/protocol/ItemServiceCallsIT.scala
+++ item-impl/src/test/scala/com/example/auction/item/protocol/ItemServiceCallsIT.scala
@@ -17,88 +17,85 @@ import play.api.Configuration
 
 class ItemServiceCallsIT extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
 
-  // FIXME: Lagom falls apart when trying to do integration tests
+  var bidEventProducerStub: ProducerStub[BidEvent] = _
 
-//  var bidEventProducerStub: ProducerStub[BidEvent] = _
-//
-//  val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra()) { ctx =>
-//    new ItemApplication(ctx) with LocalServiceLocator with TestTopicComponents {
-//
-//      val stubFactory = new ProducerStubFactory(actorSystem, materializer)
-//      bidEventProducerStub = stubFactory.producer[BidEvent]("bid-BidEvent")
-//      override lazy val biddingService: BiddingService = new BiddingServiceStub(bidEventProducerStub)
-//
-//      override def additionalConfiguration: AdditionalConfiguration = {
-//        super.additionalConfiguration ++ Configuration.from(Map(
-//          "cassandra-query-journal.eventual-consistency-delay" -> "0"
-//        ))
-//      }
-//
-//    }
-//  }
-//
-//  val itemService = server.serviceClient.implement[ItemService]
-//
-//  //import server.materializer
-//
-//  override def afterAll(): Unit = server.stop()
-//
-//  "The item service" should {
-//
-//    "allow creating items" in {
-//      //val creatorId = UUID.randomUUID()
-//      //for {
-//      //  created <- createItem(creatorId, sampleItem(creatorId))
-//      //  retrieved <- retrieveItem(created)
-//      //} yield {
-//      //  created should ===(retrieved)
-//      //}
-//      pending
-//    }
-//
-//    "return all items for a given user" in {
-//      pending
-//    }
-//
-//    "emit auction started event" in {
-//      pending
-//    }
-//
-//  }
-//
-//  // Helpers -----------------------------------------------------------------------------------------------------------
-//
-//
-//  private def sampleItem(creatorId: UUID) = {
-//    Item(
-//      id = None,
-//      creator = creatorId,
-//      itemData = ItemData(
-//        "title", "description", "USD", 10, 10, 10, None
-//      ),
-//      price = None,
-//      status = "Created",
-//      auctionStart = None, auctionEnd = None, auctionWinner = None
-//    )
-//  }
-//
-//  private def createItem(creatorId: UUID, createItem: Item) = {
-//    itemService.createItem().handleRequestHeader(ClientSecurity.authenticate(creatorId)).invoke(createItem)
-//  }
-//
-//  private def retrieveItem(item: Item) = {
-//    // TODO: ID generation was method on Item
-//    itemService.getItem(item.id.getOrElse(UUID.randomUUID())).invoke
-//  }
+  val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra()) { ctx =>
+    new ItemApplication(ctx) with LocalServiceLocator with TestTopicComponents {
+
+      val stubFactory = new ProducerStubFactory(actorSystem, materializer)
+      bidEventProducerStub = stubFactory.producer[BidEvent]("bid-BidEvent")
+      override lazy val biddingService: BiddingService = new BiddingServiceStub(bidEventProducerStub)
+
+      override def additionalConfiguration: AdditionalConfiguration = {
+        super.additionalConfiguration ++ Configuration.from(Map(
+          "cassandra-query-journal.eventual-consistency-delay" -> "0"
+        ))
+      }
+
+    }
+  }
+
+  val itemService = server.serviceClient.implement[ItemService]
+
+  //import server.materializer
+
+  override def afterAll(): Unit = server.stop()
+
+  "The item service" should {
+
+    "allow creating items" in {
+      val creatorId = UUID.randomUUID()
+      for {
+        created <- createItem(creatorId, sampleItem(creatorId))
+        retrieved <- retrieveItem(created)
+      } yield {
+        created should ===(retrieved)
+      }
+    }
+
+    "return all items for a given user" in {
+      pending
+    }
+
+    "emit auction started event" in {
+      pending
+    }
+
+  }
+
+  // Helpers -----------------------------------------------------------------------------------------------------------
+
+
+  private def sampleItem(creatorId: UUID) = {
+    Item(
+      id = None,
+      creator = creatorId,
+      itemData = ItemData(
+        "title", "description", "USD", 10, 10, 10, None
+      ),
+      price = None,
+      status = "Created",
+      auctionStart = None, auctionEnd = None, auctionWinner = None
+    )
+  }
+
+  private def createItem(creatorId: UUID, createItem: Item) = {
+    itemService.createItem().handleRequestHeader(ClientSecurity.authenticate(creatorId)).invoke(createItem)
+  }
+
+  private def retrieveItem(item: Item) = {
+    // TODO: ID generation was method on Item
+    itemService.getItem(item.id.getOrElse(UUID.randomUUID())).invoke
+  }
 
 }
 
-//class BiddingServiceStub(bidEventProducerStub: ProducerStub[BidEvent]) extends BiddingService {
-//
-//  def placeBid(itemId: UUID): ServiceCall[PlaceBid, BidResult] = ???
-//
-//  def getBids(itemId: UUID): ServiceCall[NotUsed, List[Bid]] = ???
-//
-//  def bidEvents: Topic[BidEvent] = bidEventProducerStub.topic
-//
-//}
+class BiddingServiceStub(bidEventProducerStub: ProducerStub[BidEvent]) extends BiddingService {
+
+  def placeBid(itemId: UUID): ServiceCall[PlaceBid, BidResult] = ???
+
+  def getBids(itemId: UUID): ServiceCall[NotUsed, List[Bid]] = ???
+
+  def bidEvents: Topic[BidEvent] = bidEventProducerStub.topic
+
+}
