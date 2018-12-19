fd57366ae55e7008323ee4b59e1a0678510d1262
Nico Serfontein <nserfontein@ebay.com>
2018-12-19 14:11:57 +0100

    Update

---
+++ transaction-impl/src/main/scala/com/example/auction/transaction/domain/TransactionDomain_TODO.scala
@@ -0,0 +1,23 @@
+package com.example.auction.transaction.domain
+
+import com.example.auction.transaction.impl._
+import akka.Done
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity
+
+trait TransactionDomain_TODO {
+  this: TransactionEntity with PersistentEntity =>
+
+  def transactionEventStream: TransactionEventStream
+
+  def initialState: TransactionState = ???
+
+  
+  def onGetTransaction(query: GetTransaction.type, state: TransactionState, ctx: ReadOnlyCommandContext[Option[TransactionAggregate]]): Unit = {
+  ???
+}
+
+
+  
+
+}
+
