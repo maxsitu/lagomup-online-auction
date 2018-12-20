520ff2e0e8af54a08a704e7520354229d4d35990
Nico Serfontein <nserfontein@ebay.com>
2018-12-20 09:36:40 +0100

    Update

--- transaction-impl/src/main/scala/com/example/auction/transaction/protocol/TransactionServiceCalls.scala
+++ transaction-impl/src/main/scala/com/example/auction/transaction/protocol/TransactionServiceCalls.scala
@@ -1,81 +1,113 @@
 package com.example.auction.transaction.protocol
 
-import com.example.auction.item.api._
-
-import com.example.auction.transaction.api._
-import com.example.auction.transaction.impl.TransactionPorts
-import akka.stream.scaladsl.Source
-import akka.{Done, NotUsed}
-import akka.actor.ActorSystem
-import akka.stream.Materializer
-import com.lightbend.lagom.scaladsl.pubsub.{PubSubRegistry, TopicId}
-import com.lightbend.lagom.scaladsl.server.ServerServiceCall
-import scala.concurrent.{ExecutionContext, Future}
 import java.util.UUID
+
+import akka.{Done, NotUsed}
+import com.example.auction.transaction.api._
+import com.example.auction.transaction.impl.{TransactionSummary => _, _}
+import com.example.auction.utils.ServerSecurity
+import com.lightbend.lagom.scaladsl.api.transport.NotFound
+import com.lightbend.lagom.scaladsl.server.ServerServiceCall
+import controllers.Assets
+
+import scala.concurrent.Future
 
 trait TransactionServiceCalls {
 
   val ports: TransactionPorts
 
   def _submitDeliveryDetails(userId: UUID, itemId: UUID, request: DeliveryInfo): Future[Done] = {
-  ???
-}
+    ???
+  }
 
-def _setDeliveryPrice(userId: UUID, itemId: UUID, request: Int): Future[Done] = {
-  ???
-}
+  def _setDeliveryPrice(userId: UUID, itemId: UUID, request: Int): Future[Done] = {
+    ???
+  }
 
-def _approveDeliveryDetails(userId: UUID, itemId: UUID, request: NotUsed): Future[Done] = {
-  ???
-}
+  def _approveDeliveryDetails(userId: UUID, itemId: UUID, request: NotUsed): Future[Done] = {
+    ???
+  }
 
-def _submitPaymentDetails(userId: UUID, itemId: UUID, request: PaymentInfo): Future[Done] = {
-  ???
-}
+  def _submitPaymentDetails(userId: UUID, itemId: UUID, request: PaymentInfo): Future[Done] = {
+    ???
+  }
 
-def _submitPaymentStatus(userId: UUID, itemId: UUID, request: String): Future[Done] = {
-  ???
-}
+  def _submitPaymentStatus(userId: UUID, itemId: UUID, request: String): Future[Done] = {
+    ???
+  }
 
-def _getTransaction(userId: UUID, itemId: UUID, request: NotUsed): Future[TransactionInfo] = {
-  ???
-}
+  def _getTransaction(userId: UUID, itemId: UUID, request: NotUsed): Future[TransactionInfo] = {
+    // TODO: Need current state with status
+//    ports.entityRegistry.refFor[TransactionEntity](itemId.toString)
+//      .ask(GetTransaction(userId))
+//      .map {
+//        case None =>
+//          throw NotFound(s"Transaction for item $itemId not found")
+//        case Some(aggregate) =>
+//          toApi()
+//      }
+    ???
+  }
 
-def _getTransactionsForUser(userId: UUID, status: String, pageNo: Option[String], pageSize: Option[String], request: NotUsed): Future[List[TransactionSummary]] = {
-  ???
-}
+  // Authentication ----------------------------------------------------------------------------------------------------
 
-
+  def _getTransactionsForUser(userId: UUID, status: String, pageNo: Option[String], pageSize: Option[String], request: NotUsed): Future[List[TransactionSummary]] = {
+    ???
+  }
 
   def _authenticateSubmitDeliveryDetails[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateSetDeliveryPrice[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateSetDeliveryPrice[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateApproveDeliveryDetails[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateApproveDeliveryDetails[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateSubmitPaymentDetails[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateSubmitPaymentDetails[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateSubmitPaymentStatus[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateSubmitPaymentStatus[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateGetTransaction[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateGetTransaction[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
-def _authenticateGetTransactionsForUser[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
-  ???
-}
+  def _authenticateGetTransactionsForUser[Request, Response](serviceCall: UUID => ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] = {
+    ServerSecurity.authenticated(serviceCall)
+  }
 
+  // Helpers -----------------------------------------------------------------------------------------------------------
 
+  private def toApi(aggregate: TransactionAggregate): TransactionInfo = {
+    // TODO: Need current state with status
+//    TransactionInfo(
+//      aggregate.itemId,
+//      aggregate.creator,
+//      aggregate.winner,
+//      aggregate.itemData,
+//      aggregate.itemPrice,
+//      aggregate.deliveryData.map(toApi),
+//      aggregate.deliveryPrice,
+//      aggregate.payment.map(toApi),
+//      aggregate.
+//    )
+    ???
+  }
+
+  private def toApi(deliveryData: DeliveryData): DeliveryInfo = {
+    ???
+  }
+
+  private def toApi(payment: Payment): PaymentInfo = {
+    ???
+  }
 
 }
 
--- transaction-impl/src/test/scala/com/example/auction/transaction/protocol/TransactionServiceCallsSpec.scala
+++ transaction-impl/src/test/scala/com/example/auction/transaction/protocol/TransactionServiceCallsSpec.scala
@@ -1,7 +1,145 @@
 package com.example.auction.transaction.protocol
 
-import org.scalatest.AsyncWordSpec
+import java.time.{Duration, Instant}
+import java.util.UUID
 
-class TransactionServiceCallsSpec extends AsyncWordSpec {
+import akka.{Done, NotUsed}
+import com.datastax.driver.core.utils.UUIDs
+import com.example.auction.item.api._
+import com.example.auction.transaction.api._
+import com.example.auction.transaction.impl.TransactionApplication
+import com.example.auction.utils.ClientSecurity
+import com.lightbend.lagom.scaladsl.api.{AdditionalConfiguration, ServiceCall}
+import com.lightbend.lagom.scaladsl.api.broker.Topic
+import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
+import com.lightbend.lagom.scaladsl.testkit.{ProducerStub, ProducerStubFactory, ServiceTest}
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
+import play.api.Configuration
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class TransactionServiceCallsSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll with Eventually with ScalaFutures {
+
+  var itemEventProducerStub: ProducerStub[ItemEvent] = _
+
+  val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra()) { ctx =>
+    new TransactionApplication(ctx) with LocalServiceLocator {
+
+      val stubFactory = new ProducerStubFactory(actorSystem, materializer)
+      itemEventProducerStub = stubFactory.producer[ItemEvent]("item-ItemEvent")
+
+      override val itemService: ItemService = new ItemServiceStub(itemEventProducerStub)
+
+      override def additionalConfiguration: AdditionalConfiguration =
+        super.additionalConfiguration ++ Configuration.from(Map(
+          "cassandra-query-journal.eventual-consistency-delay" -> "0",
+          "lagom.circuit-breaker.default.enabled" -> "off"
+        ))
+
+    }
+  }
+
+  override def afterAll(): Unit = server.stop()
+
+  val transactionService = server.serviceClient.implement[TransactionService]
+
+  val awaitTimeout = 10.seconds
+
+  def fixture = new {
+    val itemId = UUIDs.timeBased()
+    val creatorId = UUID.randomUUID()
+    val winnerId = UUID.randomUUID()
+    val itemPrice = 5000
+    val itemData = ItemData("title", "desc", "EUR", 1, 10, 10, None)
+    val item = Item(Some(itemId), creatorId, itemData, Some(itemPrice), "Completed", Some(Instant.now), Some(Instant.now), Some(winnerId))
+    val auctionFinished = AuctionFinished(itemId, item)
+
+    val deliveryInfo = DeliveryInfo("ADDR1", "ADDR2", "CITY", "STATE", 27, "COUNTRY")
+    val deliveryPrice = 500
+    val paymentInfo = PaymentInfo("Payment sent via wire transfer")
+
+    val transactionInfoStarted = TransactionInfo(itemId, creatorId, winnerId, itemData, itemPrice, None, None, None, "NegotiatingDelivery")
+    val transactionInfoWithDeliveryInfo = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, Some(deliveryInfo), None, None, "NegotiatingDelivery")
+    val transactionInfoWithDeliveryPrice = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, None, Some(deliveryPrice), None, "NegotiatingDelivery")
+    val transactionInfoWithPaymentPending = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, Some(deliveryInfo), Some(deliveryPrice), None, "PaymentPending")
+    val transactionInfoWithPaymentDetails = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, Some(deliveryInfo), Some(deliveryPrice), Some(paymentInfo), "PaymentSubmitted")
+    val transactionInfoWithPaymentApproved = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, Some(deliveryInfo), Some(deliveryPrice), Some(paymentInfo), "PaymentConfirmed")
+    val transactionInfoWithPaymentRejected = TransactionInfo(itemId, creatorId, winnerId, itemData, item.price.get, Some(deliveryInfo), Some(deliveryPrice), Some(paymentInfo), "PaymentPending")
+  }
+
+  "The transaction service" should {
+
+    "create transaction on auction finished" in {
+//      val f = fixture
+//      itemEventProducerStub.send(f.auctionFinished)
+//      eventually(timeout(Span(10, Seconds))) {
+//        retrieveTransaction(f.itemId, f.creatorId) should ===(f.transactionInfoStarted)
+//      }
+
+      // TODO: need current state with status
+      pending
+    }
+
+    "not create transaction with no winner" in {
+      pending
+    }
+
+    "submit delivery details" in {
+      pending
+    }
+
+    "set delivery price" in {
+      pending
+    }
+
+    "approve delivery details" in {
+      pending
+    }
+
+    "forbid approve empty delivery details" in {
+      pending
+    }
+
+    "submit payment details" in {
+      pending
+    }
+
+    "approve payment" in {
+      pending
+    }
+
+    "reject payment" in {
+      pending
+    }
+
+  }
+
+  // Helpers -----------------------------------------------------------------------------------------------------------
+
+  private def retrieveTransaction(itemId: UUID, creatorId: UUID): TransactionInfo = {
+    Await.result(
+      transactionService.getTransaction(itemId)
+        .handleRequestHeader(ClientSecurity.authenticate(creatorId))
+        .invoke(),
+      awaitTimeout
+    )
+  }
+
+}
+
+class ItemServiceStub(itemEventProducerStub: ProducerStub[ItemEvent]) extends ItemService {
+
+  def createItem(): ServiceCall[Item, Item] = ???
+
+  def startAuction(id: UUID): ServiceCall[NotUsed, Done] = ???
+
+  def getItem(id: UUID): ServiceCall[NotUsed, Item] = ???
+
+  def getItemsForUser(id: UUID, status: String, page: Option[String]): ServiceCall[NotUsed, ItemSummaryPagingState] = ???
+
+  def itemEvents: Topic[ItemEvent] = itemEventProducerStub.topic
 
 }
